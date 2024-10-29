033306aabc6771f766fd68cf0d51c90520c39475
1wes <okemwawes@gmail.com>
2024-10-30 00:20:18 +0300

    feat(axios): change axios instance base url

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://sil-ta-api.onrender.com",
+    baseURL: "https://jsonplaceholder.typicode.com/",
     withCredentials:true
 });
 
